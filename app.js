de8f0f723ebd14ebdda46bcf170a30394e0fc2be
patbail <pbaillairge@gmail.com>
2025-10-23 17:08:34 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const nameCategory = [encampment, person, dungeon];
+
+const encampment = {
+	caracteristic: ['Tiny', 'Small', 'Modest', 'Bustling', 'Large', 'Sprawling', 'Fortified', 'Hidden'],
+	type: ['hamlet', 'village', 'town', 'city', 'stronghold', 'outpost', 'camp', 'settlement', 'refuge'],
+	name: ['Thumsberg', 'Eldhollow', 'Ravenmoor', 'Stonewatch', 'Brimvale', 'Frostmere', 'Oakshade', 'Duskreach', 'Ironford', 'Windmere']
+}
+
+const person = {
+	firsName: ['Alaric', 'Branwen', 'Cedric', 'Elowen', 'Gareth', 'Isolde', 'Kaelen', 'Liora', 'Thorne', 'Ysolde'],
+	ladName: ['Darkmoor', 'Ironhart', 'Windrider', 'Stormborn', 'Emberfall', 'Ravenshade', 'Thistlekeep', 'Grimvale', 'Silverbranch', 'Dreadmere']
+}
+const dungeon = {
+	type: ['Crypt', 'Cave', 'Grotto', 'Cellar', 'Dungeon', 'Tower', 'Sanctum', 'Labyrinth', 'Catacombs', 'Ruins'],
+	Adjective: ['of the Forsaken', 'of the Verdant', 'of Eternal Flame', 'of the Divine', 'of Shadows', 'of the Whispering', 'of the Blighted', 'of the Lost', 'of the Hollow', 'of the Cursed'],
+	theme: ['Light', 'Forest', 'Gods', 'Mountain', 'Ashen Vale', 'Moonspire', 'Thornreach', 'Ebonhold', 'Starfall', 'Frostpeak']
+}
